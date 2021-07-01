89bf6c9cc2a42a4a03da7c001e7502bc67903782
taoxinhua <taoxinhua@xzintl.com>
2021-07-01 18:15:12 +0800

    ext vue

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default {
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['axios'],
+      external: ['axios', 'vue'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
